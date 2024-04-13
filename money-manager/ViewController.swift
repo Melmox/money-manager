dcda72d16d3b75fa7d30e04fa874e9bba3c24fb5
Melnyk Maksym <melnyk.maxim.dp@gmail.com>
2024-04-13 22:29:04 +0300

    create and base setup project

---
+++ money-manager/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  money-manager
+//
+//  Created by developer on 13.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
